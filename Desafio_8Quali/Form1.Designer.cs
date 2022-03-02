fdb27960723426c362546fa67debae2faaa90195
Pablo Machado <pablo.henriquee1@hotmail.com>
2022-03-02 15:00:30 -0300

    Desafio 8Quali

---
+++ Desafio_8Quali/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿
+namespace Desafio_8Quali
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmpresa = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtTelefone1 = new System.Windows.Forms.MaskedTextBox();
+            this.txtTelefone2 = new System.Windows.Forms.MaskedTextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(254, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Desafio 8Quali";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(21, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(21, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Empresa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(392, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(392, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Telefone Pessoal";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(392, 164);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(141, 21);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Telefone Comercial";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(110, 114);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(227, 23);
+            this.txtNome.TabIndex = 6;
+            // 
+            // txtEmpresa
+            // 
+            this.txtEmpresa.Location = new System.Drawing.Point(110, 160);
+            this.txtEmpresa.Name = "txtEmpresa";
+            this.txtEmpresa.Size = new System.Drawing.Size(227, 23);
+            this.txtEmpresa.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(454, 64);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(227, 23);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.AllowUserToAddRows = false;
+            this.dgvDados.AllowUserToDeleteRows = false;
+            this.dgvDados.AllowUserToOrderColumns = true;
+            this.dgvDados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dgvDados.BackgroundColor = System.Drawing.Color.White;
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvDados.Location = new System.Drawing.Point(21, 242);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.RowTemplate.Height = 25;
+            this.dgvDados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDados.Size = new System.Drawing.Size(660, 207);
+            this.dgvDados.TabIndex = 11;
+            this.dgvDados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDados_CellClick);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.White;
+            this.btnSalvar.ForeColor = System.Drawing.Color.Green;
+            this.btnSalvar.Location = new System.Drawing.Point(21, 205);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(92, 31);
+            this.btnSalvar.TabIndex = 12;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.White;
+            this.btnEditar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnEditar.Location = new System.Drawing.Point(132, 205);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(92, 31);
+            this.btnEditar.TabIndex = 13;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.Color.White;
+            this.btnExcluir.ForeColor = System.Drawing.Color.Maroon;
+            this.btnExcluir.Location = new System.Drawing.Point(245, 205);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(92, 31);
+            this.btnExcluir.TabIndex = 14;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.BackColor = System.Drawing.Color.White;
+            this.btnConsultar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnConsultar.Location = new System.Drawing.Point(353, 205);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(92, 31);
+            this.btnConsultar.TabIndex = 15;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = false;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.BackColor = System.Drawing.Color.White;
+            this.btnExibir.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnExibir.Location = new System.Drawing.Point(468, 205);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(92, 31);
+            this.btnExibir.TabIndex = 16;
+            this.btnExibir.Text = "Listar";
+            this.btnExibir.UseVisualStyleBackColor = false;
+            this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.AutoSize = true;
+            this.txtId.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtId.ForeColor = System.Drawing.Color.White;
+            this.txtId.Location = new System.Drawing.Point(21, 62);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(25, 21);
+            this.txtId.TabIndex = 17;
+            this.txtId.Text = "ID";
+            this.txtId.UseMnemonic = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 23);
+            this.textBox1.TabIndex = 18;
+            // 
+            // txtTelefone1
+            // 
+            this.txtTelefone1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtTelefone1.Location = new System.Drawing.Point(560, 110);
+            this.txtTelefone1.Mask = "(00)    0 0000-0000";
+            this.txtTelefone1.Name = "txtTelefone1";
+            this.txtTelefone1.Size = new System.Drawing.Size(121, 25);
+            this.txtTelefone1.TabIndex = 19;
+            // 
+            // txtTelefone2
+            // 
+            this.txtTelefone2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtTelefone2.Location = new System.Drawing.Point(562, 160);
+            this.txtTelefone2.Mask = "(00)    0 0000-0000";
+            this.txtTelefone2.Name = "txtTelefone2";
+            this.txtTelefone2.Size = new System.Drawing.Size(119, 25);
+            this.txtTelefone2.TabIndex = 20;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.White;
+            this.btnLimpar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnLimpar.Location = new System.Drawing.Point(589, 205);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(92, 31);
+            this.btnLimpar.TabIndex = 21;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(704, 461);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtTelefone2);
+            this.Controls.Add(this.txtTelefone1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtEmpresa);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Desafio 8Quali";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmpresa;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.DataGridView dgvDados;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnExibir;
+        private System.Windows.Forms.Label txtId;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MaskedTextBox txtTelefone1;
+        private System.Windows.Forms.MaskedTextBox txtTelefone2;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ Desafio_8Quali/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Desafio_8Quali
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conexao;
+        MySqlCommand comando;
+        MySqlDataAdapter da;
+        MySqlDataReader dr;
+        
+        string strSQL;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new MySqlConnection("Server = localhost; Database = desafio; Uid = root; Pwd = 99798950;");
+
+                if (txtNome.Text != "")
+                {
+                    strSQL = "INSERT INTO CADASTRO_DESAFIO (NOME, EMPRESA, EMAIL, TELEFONE_PESSOAL, TELEFONE_COMERCIAL) VALUES (@NOME, @EMPRESA, @EMAIL, @TELEFONE_PESSOAL, @TELEFONE_COMERCIAL)";
+
+                    comando = new MySqlCommand(strSQL, conexao);
+                    comando.Parameters.AddWithValue("@NOME", txtNome.Text);
+                    comando.Parameters.AddWithValue("@EMPRESA", txtEmpresa.Text);
+                    comando.Parameters.AddWithValue("@EMAIL", txtEmail.Text);
+                    comando.Parameters.AddWithValue("@TELEFONE_PESSOAL", txtTelefone1.Text);
+                    comando.Parameters.AddWithValue("@TELEFONE_COMERCIAL", txtTelefone2.Text);
+
+                    conexao.Open();
+                    comando.ExecuteNonQuery();
+                    MessageBox.Show("Cadastrado com Sucesso!");
+                }
+                else
+                {
+                    MessageBox.Show("Insira o Nome");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+            }
+
+            txtNome.Clear();
+            txtEmpresa.Clear();
+            txtEmail.Clear();
+            txtTelefone1.Clear();
+            txtTelefone2.Clear();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new MySqlConnection("Server = localhost; Database = desafio; Uid = root; Pwd = 99798950;");
+
+                strSQL = "UPDATE CADASTRO_DESAFIO SET NOME = @NOME, EMPRESA = @EMPRESA, EMAIL = @EMAIL, TELEFONE_PESSOAL = @TELEFONE_PESSOAL, TELEFONE_COMERCIAL = @TELEFONE_COMERCIAL WHERE ID = @ID";
+                comando = new MySqlCommand(strSQL, conexao);
+                comando.Parameters.AddWithValue("@ID", textBox1.Text);
+                comando.Parameters.AddWithValue("@NOME", txtNome.Text);
+                comando.Parameters.AddWithValue("@EMPRESA", txtEmpresa.Text);
+                comando.Parameters.AddWithValue("@EMAIL", txtEmail.Text);
+                comando.Parameters.AddWithValue("@TELEFONE_PESSOAL", txtTelefone1.Text);
+                comando.Parameters.AddWithValue("@TELEFONE_COMERCIAL", txtTelefone2.Text);
+
+                conexao.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Atualizado com Sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+            }
+
+            txtNome.Clear();
+            txtEmpresa.Clear();
+            txtEmail.Clear();
+            txtTelefone1.Clear();
+            txtTelefone2.Clear();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new MySqlConnection("Server = localhost; Database = desafio; Uid = root; Pwd = 99798950;");
+
+                if (textBox1.Text != "")
+                {
+                    if (MessageBox.Show("Tem certeza que deseja excluir este cadastro?", "Excluir Cadastro", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        strSQL = "DELETE FROM CADASTRO_DESAFIO WHERE ID = @ID";
+                        comando = new MySqlCommand(strSQL, conexao);
+                        comando.Parameters.AddWithValue("@ID", textBox1.Text);
+
+                        conexao.Open();
+                        comando.ExecuteNonQuery();
+                        MessageBox.Show("Deletado com Sucesso!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Informe o ID que deseja excluir.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+            }
+
+            txtNome.Clear();
+            txtEmpresa.Clear();
+            txtEmail.Clear();
+            txtTelefone1.Clear();
+            txtTelefone2.Clear();
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            try
+            {                
+                conexao = new MySqlConnection("Server = localhost; Database = desafio; Uid = root; Pwd = 99798950;");
+                strSQL = "SELECT * FROM cadastro_desafio WHERE id = @ID OR nome = @NOME OR empresa = @EMPRESA OR email = @EMAIL OR telefone_pessoal = @TELEFONE_PESSOAL OR telefone_comercial = @TELEFONE_COMERCIAL";
+                comando = new MySqlCommand(strSQL, conexao);
+                
+                comando.Parameters.AddWithValue("@ID", textBox1.Text);
+                comando.Parameters.AddWithValue("@NOME", txtNome.Text);
+                comando.Parameters.AddWithValue("@EMPRESA", txtEmpresa.Text);
+                comando.Parameters.AddWithValue("@EMAIL", txtEmail.Text);
+                comando.Parameters.AddWithValue("@TELEFONE_PESSOAL", txtTelefone1.Text);
+                comando.Parameters.AddWithValue("@TELEFONE_COMERCIAL", txtTelefone2.Text);
+
+                conexao.Open();
+                dr = comando.ExecuteReader();
+
+                while (dr.Read())
+                {
+
+                    textBox1.Text = Convert.ToString(dr["Id"]);
+                    txtNome.Text = Convert.ToString(dr["Nome"]);
+                    txtEmpresa.Text = Convert.ToString(dr["Empresa"]);
+                    txtEmail.Text = Convert.ToString(dr["Email"]);
+                }
+               
+            
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+            }
+            conexao = new MySqlConnection("Server = localhost; Database = desafio; Uid = root; Pwd = 99798950;");
+            strSQL = "SELECT * FROM cadastro_desafio WHERE id = '" + textBox1.Text + "' OR nome = '" + txtNome.Text + "' OR empresa = '" + txtEmpresa.Text + "' OR email = '" + txtEmail.Text + "' OR telefone_pessoal = '" + txtTelefone1.Text + "' OR telefone_comercial = '" + txtTelefone2.Text + "'";
+            da = new MySqlDataAdapter(strSQL, conexao);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgvDados.DataSource = dt;
+            
+        }
+
+        //Listar
+        private void btnExibir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new MySqlConnection("Server = localhost; Database = desafio; Uid = root; Pwd = 99798950;");
+
+                strSQL = "SELECT * FROM CADASTRO_DESAFIO";
+
+                da = new MySqlDataAdapter(strSQL, conexao);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvDados.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comando = null;
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            txtNome.Clear();
+            txtEmpresa.Clear();
+            txtEmail.Clear();
+            txtTelefone1.Clear();
+            txtTelefone2.Clear();
+        }
+
+        private void dgvDados_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow linha = dgvDados.Rows[e.RowIndex];
+            textBox1.Text = linha.Cells[0].Value.ToString();
+            txtNome.Text = linha.Cells[1].Value.ToString();
+            txtEmpresa.Text = linha.Cells[2].Value.ToString();
+            txtEmail.Text = linha.Cells[3].Value.ToString();
+            txtTelefone1.Text = linha.Cells[4].Value.ToString();
+            txtTelefone2.Text = linha.Cells[5].Value.ToString();
+        }
+    }
+}
